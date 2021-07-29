6fc2c040df23facfdbee3a31171e01485e09e17b
Oleg Valter <o.a.philippov@gmail.com>
2021-07-30 00:43:00 +0300

    0.1.0

--- dist/headers.js
+++ dist/headers.js
@@ -13,5 +13,5 @@
 // @namespace       userscripters
 // @source          git+https://github.com/userscripters/bring-back-404.git
 // @supportURL      https://github.com/userscripters/bring-back-404/issues
-// @version         0.0.5
+// @version         0.1.0
 // ==/UserScript==
--- dist/modern/index.js
+++ dist/modern/index.js
@@ -13,7 +13,7 @@
 // @namespace       userscripters
 // @source          git+https://github.com/userscripters/bring-back-404.git
 // @supportURL      https://github.com/userscripters/bring-back-404/issues
-// @version         0.0.5
+// @version         0.1.0
 // ==/UserScript==
 
 "use strict";
