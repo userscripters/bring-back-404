527d8b6eb080697df69d35d66548a8bd9dc84c8a
Oleg Valter <o.a.philippov@gmail.com>
2021-07-29 22:17:04 +0300

    initial version

---
+++ src/index.ts
@@ -0,0 +1,70 @@
+((_w, d, l) => {
+    type NotFoundOptions = {
+        site: string;
+        imageURL: string;
+    };
+
+    const pageNotFounds: NotFoundOptions[] = [
+        {
+            site: "stackoverflow",
+            imageURL: "https://i.stack.imgur.com/okzBE.png",
+        },
+        {
+            site: "meta.stackoverflow",
+            imageURL: "https://i.stack.imgur.com/0i2to.png",
+        },
+        {
+            site: "askubuntu",
+            imageURL: "https://i.stack.imgur.com/KopoQ.png",
+        },
+        {
+            site: "superuser",
+            imageURL: "https://i.stack.imgur.com/hMfYx.png",
+        },
+    ];
+
+    const { hostname } = l;
+
+    const currentSite = hostname.split(".").slice(0, -1).join(".");
+
+    const config = pageNotFounds.find(({ site }) => site === currentSite);
+    if (!config) return console.debug(`not on supported site: ${currentSite}`);
+
+    const { imageURL } = config;
+
+    const image = d.createElement("img");
+    image.src = imageURL;
+    image.alt = "Page not found";
+    image.decoding = "async";
+    image.width = 250;
+    image.style.display = "none";
+
+    image.addEventListener("error", () =>
+        console.debug(`failed to load 404 image on ${currentSite}`)
+    );
+
+    image.addEventListener("load", () => {
+        const contentModal = d.getElementById("content");
+        if (!contentModal) return console.debug("missing content modal");
+
+        const alertImage = contentModal.querySelector("svg.spotAlertXL");
+        if (!alertImage) return console.debug("missing 404 image");
+
+        alertImage.replaceWith(image);
+        image.style.display = "unset";
+
+        const header = contentModal.querySelector("h1");
+        if (!header) return console.debug("missing 404 header");
+
+        //removes excessive top margin before header
+        header.classList.remove("mt48");
+
+        const flexWrap = header.closest(".d-flex");
+        if (!flexWrap) return console.debug("404 content does't use Flexbox");
+
+        //properly centers flex items vertically
+        flexWrap.classList.replace("ai-start", "ai-center");
+    });
+
+    d.body.append(image);
+})(window, document, location);
