c7a319fef2d5a452d8b0b73fd220c5bc635ba0a2
Oleg Valter <o.a.philippov@gmail.com>
2021-08-09 07:19:00 +0300

    0.7.0

--- dist/headers.js
+++ dist/headers.js
@@ -13,5 +13,5 @@
 // @namespace       userscripters
 // @source          git+https://github.com/userscripters/bring-back-404.git
 // @supportURL      https://github.com/userscripters/bring-back-404/issues
-// @version         0.6.1
+// @version         0.7.0
 // ==/UserScript==
